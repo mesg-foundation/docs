7932e9514ddbd5c33bcb7dd7476a7f465ececfb7
Anthony ESTEBE <anthony@mesg.com>
2019-07-11 09:50:49 +0700

    stream and anchor

--- .vuepress/components/api-doc.vue
+++ .vuepress/components/api-doc.vue
@@ -1,17 +1,22 @@
 <template>
   <section>
     <p v-html="fromMD(api.description)"></p>
+    <ul>
+      <li v-for="method in api.methods" :key="method.name">
+        <a :href="`#${method.name}`">{{method.name}}</a>
+      </li>
+    </ul>
     <div v-for="method in api.methods" :key="method.name">
-      <h2>{{method.name}}</h2>
+      <h2 :id="method.name">{{method.name}}</h2>
       <p v-html="fromMD(method.description)"></p>
       <v-tabs>
         <v-tab title="Request">
-          <h4>{{method.requestType}}</h4>
+          <h4><Badge v-if="method.requestStreaming" text="stream" type="warn"/> {{method.requestType}}</h4>
           <p v-html="fromMD(types[method.requestFullType].description)"></p>
           <v-param-table :parameter="types[method.requestFullType]" :types="types" />
         </v-tab>
-        <v-tab title="Reply">
-          <h4>{{method.replyType}}</h4>
+        <v-tab title="Response">
+          <h4><Badge v-if="method.responseStreaming" text="stream" type="warn"/> {{method.responseType}}</h4>
           <p v-html="fromMD(types[method.responseFullType].description)"></p>
           <v-param-table :parameter="types[method.responseFullType]" :types="types" />
         </v-tab>
